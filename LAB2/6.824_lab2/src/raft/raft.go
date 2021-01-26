7bd0300cd8763512f5cfe52bc4d85b096018878e
tavidong <tavidong@tencent.com>
2021-01-26 20:21:52 +0800

    change comment

--- LAB2/6.824_lab2/src/raft/raft.go
+++ LAB2/6.824_lab2/src/raft/raft.go
@@ -1,9 +1,8 @@
 package raft
 
 //
-// this is an outline of the API that raft must expose to
-// the service (or tester). see comments below for
-// each of these functions for more details.
+// this is an outline of the API that raft must expose to the service (or tester).
+// see comments below for each of these functions for more details.
 //
 // rf = Make(...)
 //   create a new Raft server.
@@ -13,8 +12,7 @@ package raft
 //   ask a Raft for its current term, and whether it thinks it is leader
 // ApplyMsg
 //   each time a new entry is committed to the log, each Raft peer
-//   should send an ApplyMsg to the service (or tester)
-//   in the same server.
+//   should send an ApplyMsg to the service (or tester) in the same server.
 //
 
 import (
@@ -34,9 +32,8 @@ import (
 // CommandValid to true to indicate that the ApplyMsg contains a newly
 // committed log entry.
 //
-// in Lab 3 you'll want to send other kinds of messages (e.g.,
-// snapshots) on the applyCh; at that point you can add fields to
-// ApplyMsg, but set CommandValid to false for these other uses.
+// in Lab 3 you'll want to send other kinds of messages (e.g., snapshots) on the applyCh;
+// at that point you can add fields to ApplyMsg, but set CommandValid to false for these other uses.
 //
 type ApplyMsg struct {
 	CommandValid bool
@@ -55,13 +52,11 @@ type Raft struct {
 	dead      int32               // set by Kill()
 
 	// Your data here (2A, 2B, 2C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
+	// Look at the paper's Figure 2 for a description of what state a Raft server must maintain.
 
 }
 
-// return currentTerm and whether this server
-// believes it is the leader.
+// return currentTerm and whether this server believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
 
 	var term int

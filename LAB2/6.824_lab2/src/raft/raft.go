05962439543db60f9a835e478d7b7c830afd62dd
tavidong <tavidong@tencent.com>
2021-02-16 22:57:27 +0800

    fix

--- LAB2/6.824_lab2/src/raft/raft.go
+++ LAB2/6.824_lab2/src/raft/raft.go
@@ -425,7 +425,7 @@ func (rf *Raft) SendHeatBeat(term int) {
 					atomic.StoreInt32(&reqDone, 1)
 					wg.Done()
 				}
-				if atomic.LoadInt32(&reqDone) == 0 && ret && reply.Term > rf.GetTerm() {
+				if ret && reply.Success == false && reply.Term > rf.GetTerm() {
 					rf.ChangeSelfTerm(reply.Term)
 					rf.SetTermState(reply.Term, PEER_STATE_FOLLOWER)
 					return
